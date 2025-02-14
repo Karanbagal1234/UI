f754783bc8462e13a5a52924d132088a230b7a6a
Karanbagal1234 <karanbagal021@gmail.com>
2025-02-15 00:22:53 +0530

    Updates

---
+++ assets/AuthChecker-BxEdAQ6N.js
@@ -0,0 +1,1 @@
+import{f as t}from"./index-BWStPa2h.js";const s=()=>{const r=t();return{redirect:e=>{e&&r("/")},redirectTo:e=>{e&&(console.log(e),r(e))}}};export{s as u};
---
+++ assets/Model-CNCi-wLF.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-BWStPa2h.js";import{c as o,m as c}from"./createLucideIcon-BqLr22Er.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],r=o("X",l),d=({isOpen:t,onClose:a,title:s="",children:i})=>t?e.jsx("div",{className:"fixed inset-0 bg-[#000000ad] bg-opacity-50 flex justify-center items-center z-50 p-4",children:e.jsxs(c.div,{initial:{opacity:0,scale:.9},animate:{opacity:1,scale:1},exit:{opacity:0,scale:.9},className:"bg-white p-6 rounded-2xl shadow-xl w-full max-w-md relative",children:[e.jsx("button",{onClick:a,className:"absolute top-3 right-3 text-gray-500 hover:text-gray-700",children:e.jsx(r,{size:20})}),e.jsx("h2",{className:"text-2xl font-semibold text-gray-800 mb-4 text-center",children:s}),e.jsx("div",{children:i})]})}):null;export{d as M};
